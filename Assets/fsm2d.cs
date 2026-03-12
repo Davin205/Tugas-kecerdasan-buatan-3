32428fbface66826900d09d5ae1cdc2ac409a7e7
Davin205 <132825351+Davin205@users.noreply.github.com>
2026-03-12 10:25:49 +0700

    project start

---
+++ Assets/fsm2d.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fsm2d : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum State
+    {
+        Idle,
+        Chase,
+        Attack,
+    }
+
+public State currentState;
+public Transform player;
+public float chaseDistance = 3f;
+public float attackDistance = 2f;
+public float speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        switch (currentState)
+        {
+            case State.Idle:
+                if (distance < chaseDistance)
+                {currentState = State.Chase;
+                }
+                break;
+            case State.Chase:
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+                if (distance < attackDistance)
+                {
+                    currentState = State.Attack;
+                }
+                break;
+            case State.Attack:
+                Debug.Log("enemy attack on");
+                if (distance > attackDistance)
+                {
+                    currentState = State.Chase;
+                }
+                break;
+        }
+    }
+}
